401b7410c03e78bb5dd57c301a8939459f67868d
thuyle-23 <thuyltt@ctnpsolutions.com>
2023-04-30 23:08:14 +0700

    ...

---
+++ app/src/main/java/com/example/fooddelivery/ForgetPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_forget_password);
+    }
+}
