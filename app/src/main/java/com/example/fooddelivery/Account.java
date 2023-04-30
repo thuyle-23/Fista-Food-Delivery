8cb2957d5adc3733d2192ee8f2330ad9a3d97a21
thuyle-23 <thuyltt@ctnpsolutions.com>
2023-04-30 11:57:55 +0700

    ...

---
+++ app/src/main/java/com/example/fooddelivery/Account.java
@@ -0,0 +1,13 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_account);
+    }
+}
