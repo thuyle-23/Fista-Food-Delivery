b81a4b2ece6f2e480fed3c2b1905765e64afdfff
thuyle-23 <thuyltt@ctnpsolutions.com>
2023-04-30 21:13:47 +0700

    temp

--- app/src/main/java/com/example/fooddelivery/Login.java
+++ app/src/main/java/com/example/fooddelivery/Login.java
@@ -4,20 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    private EditText txtphoneNumber;
+    private Button btnGetOTP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
 
-        final EditText txtphoneNumber = findViewById(R.id.txtphoneNumber);
-        Button btnGetOTP = findViewById(R.id.btnGetOTP);
+        txtphoneNumber = findViewById(R.id.txtphoneNumber);
+        btnGetOTP = findViewById(R.id.btnGetOTP);
+
+        txtphoneNumber.addTextChangedListener(loginTextWatcher);
 
         btnGetOTP.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,4 +37,19 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String phoneNumber = txtphoneNumber.getText().toString().trim();
+
+            btnGetOTP.setEnabled(!phoneNumber.isEmpty() && phoneNumber.length() == 9);
+        }
+        @Override public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
--- app/src/main/java/com/example/fooddelivery/SignIn.java
+++ app/src/main/java/com/example/fooddelivery/SignIn.java
@@ -2,28 +2,13 @@ package com.example.fooddelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
-    private TextView txtPhoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_sign_in);
-
-        txtPhoneNumber = (TextView) findViewById(R.id.txtPhoneNumber);
-        txtPhoneNumber.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                openLoginPage();
-            }
-        });
-    }
-    public void openLoginPage(){
-        Intent intent = new Intent(SignIn.this, Login.class);
-        startActivity(intent);
     }
 }
