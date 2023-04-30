58e74c85c3bc5e5673c6d12cdc08f37a4212381b
thuyle-23 <thuyltt@ctnpsolutions.com>
2023-04-30 21:46:15 +0700

    send_otp

--- app/src/main/java/com/example/fooddelivery/Login.java
+++ app/src/main/java/com/example/fooddelivery/Login.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,14 +13,17 @@ import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    private EditText txtphoneNumber;
+    private Button btnGetOTP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
 
-        final EditText txtphoneNumber = findViewById(R.id.txtphoneNumber);
-        Button btnGetOTP = findViewById(R.id.btnGetOTP);
+        txtphoneNumber = findViewById(R.id.txtphoneNumber);
+        btnGetOTP = findViewById(R.id.btnGetOTP);
 
+        txtphoneNumber.addTextChangedListener(loginTextWatcher);
         btnGetOTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,4 +37,19 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String phoneNumber = txtphoneNumber.getText().toString().trim();
+
+            btnGetOTP.setEnabled(!phoneNumber.isEmpty() && phoneNumber.length() == 9);
+        }
+        @Override public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
