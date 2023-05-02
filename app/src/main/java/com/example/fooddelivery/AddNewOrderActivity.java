ec311d060ab08ec2f4466de2d2aca18401975358
thuyle-23 <thuyltt@ctnpsolutions.com>
2023-05-02 12:53:21 +0700

    add order_store information

---
+++ app/src/main/java/com/example/fooddelivery/AddNewOrderActivity.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_add_new_order);
+    }
+}
---
+++ app/src/main/java/com/example/fooddelivery/StoreInformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_store_information);
+    }
+}
