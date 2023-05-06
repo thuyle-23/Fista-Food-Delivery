67c65b3081aae5ea506ec204c9db8e0d39b209f8
19520814_phuongnhi <19520814@gm.uit.edu.vn>
2023-05-06 20:12:56 +0700

    2 man hinh

---
+++ app/src/main/java/com/example/fooddelivery/Rating_and_Review.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rating_and_Review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_rating_and_review);
+    }
+}
